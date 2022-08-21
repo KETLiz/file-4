c6632f66843a9d8dbd12f00bb8ae0c949840be1f
liza <varlamova_e.t@mail.ru>
2022-08-21 12:52:40 +0300

    4 задача

---
+++ y4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+        i++;
+}
+
